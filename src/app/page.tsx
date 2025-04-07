7a7133ae76418ce8f411e7677b3cf7a63222da0b
Guillaume Picard <gwpicard@gmail.com>
2025-04-07 15:02:11 +0200

    change main tagline, make spacing full heigh home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center">
-            <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-                <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+            <section className="flex min-h-[100vh] w-full items-center justify-center">
+                <div className="container flex max-w-[64rem] flex-col items-center gap-8 text-center">
                     <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-                        I help teams turn data-driven ideas into working tools. Fast.
+                        I help teams turn data-driven ideas into real solutions.
                     </h1>
                     <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
                         From strategy and insights to fully functional prototypes, I'll help you unlock the power of your data every step of the way.
