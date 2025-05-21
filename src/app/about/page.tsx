119bb81d38810cc98520d89c14a0d4fc6a913f9d
Guillaume Picard <gwpicard@gmail.com>
2025-05-21 10:45:47 +0200

    improve formatting and text about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,18 +3,20 @@ import { Button } from "@/components/ui/button"
 
 export default function About() {
     return (
-        <div className="container py-10">
-            <div className="mx-auto max-w-[58rem] space-y-12">
-                <div className="space-y-6">
+        <div className="container flex min-h-[calc(100vh-12rem)] items-center py-16">
+            <div className="mx-auto max-w-[58rem] space-y-16">
+                <div className="space-y-8">
                     <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
                         About Me
                     </h1>
-                    <p className="text-xl text-muted-foreground">
-                        I'm Guillaume, a data science consultant who loves turning ideas into real, working tools. My background blends analytics, machine learning, and software engineering, and I've worked across industries like energy, finance, and manufacturing—always focusing on practical, user-friendly solutions. I believe in starting small, testing fast, and iterating based on real feedback, because if something doesn't bring clear value, we re-think it. Whether you're a startup exploring AI or an established team seeking better data insights, I'm here to help make it happen quickly and effectively.
+                    <p className="text-xl leading-relaxed text-muted-foreground">
+                        Hi! 👋🏼 I'm Guillaume, a data science and AI specialist who loves turning ideas into real, useful tools. My skillset blends analytics, ML & AI, and software engineering. I've worked across industries and organisations of all sizes—always focusing on practical, user-centred solutions. <br></br>
+                        <br></br>
+                        I believe in starting small, testing fast, and iterating based on real feedback, because if something doesn't bring clear value, we re-think it. Whether you're a startup exploring AI or an established team seeking better data insights, I'm here to help make it happen quickly and effectively.
                     </p>
                 </div>
 
-                <div className="flex justify-center space-x-4">
+                <div className="flex justify-center space-x-4 pt-6">
                     <Link href="/contact">
                         <Button size="lg">Send me a message</Button>
                     </Link>
